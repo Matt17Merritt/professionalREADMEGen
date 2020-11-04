5f01900f4cc5f959b1b5e6776952211a77869675
Matt Merritt <matt17merritt@gmail.com>
2020-11-04 14:20:15 -0500

    rearranged file structure

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('Hello Node!')
+// declaring variables and dependencies
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
