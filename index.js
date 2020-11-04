72759775ddfc2fdf71dc31a836c4a5815506d481
Matt Merritt <matt17merritt@gmail.com>
2020-11-04 15:32:42 -0500

    setup inquirer object

--- index.js
+++ index.js
@@ -3,3 +3,68 @@ console.log('Hello Node!')
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown")
+
+// prompt user for questions
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message:"Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contibutors of this project?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+}
